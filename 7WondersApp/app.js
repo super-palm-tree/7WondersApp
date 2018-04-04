3b64912886874537f1db61288910de0a0a95139a
peridotclod <maeby@outlook.com>
2018-04-03 20:19:38 -0400

    Add project files.

---
+++ 7WondersApp/app.js
@@ -0,0 +1,46 @@
+﻿$(document).ready(function () {
+    var scienceCategories = 3;
+    var categories = 7;
+    var players = 8;
+
+    var scienceScores = new Array(scienceCategories);
+    var scienceScoreNames = ["tablets", "compasses", "gears"];
+
+    var score = 0;
+    var scoreNames = ["battle", "money", "wonder", "civilian", "commerce", "sciencetotal", "guild"];
+
+    $("#calculate").click(function () {
+        for (var i = 0; i < players; i++) {
+            var playerNumber = "#player" + (i + 1);
+         
+            var total = calculatePlayerScore(playerNumber);
+
+            $(playerNumber + "total").val(total);
+        }
+
+    });
+
+    $(".science-row").click(function () {
+        $(".scienceItems-row").slideToggle();
+    });
+
+    function calculatePlayerScore(playerNumber) {
+        calculatePlayerScienceScore(playerNumber);
+        var total = 0;
+        for (var j = 0; j < categories; j++) {
+            score = +$(playerNumber + scoreNames[j]).val();
+            total += score;
+        }
+        return total;
+    }
+
+    function calculatePlayerScienceScore(playerNumber) {
+        var scienceTotal = 0;
+        for (var k = 0; k < scienceCategories; k++) {
+            scienceScores[k] = +$(playerNumber + scienceScoreNames[k]).val();
+            scienceTotal += (scienceScores[k] * scienceScores[k]);
+        }
+        scienceTotal += (Math.min(...scienceScores) * 7);
+        $(playerNumber + "sciencetotal").val(scienceTotal);
+    }
+});
