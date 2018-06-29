5bf68f7e237dc5ce8f46c9b4aebbb14affc949f8
super-palm-tree <maeby@outlook.com>
2018-06-29 10:39:31 -0400

    Added player number selector.

--- 7WondersApp/app.js
+++ 7WondersApp/app.js
@@ -1,19 +1,24 @@
 ﻿$(document).ready(function () {
     var scienceCategories = 3;
     var categories = 7;
-    var players = 7;
+    var currentPlayers = 7;
+    var maxPlayers = 7;
 
     var scienceScores = new Array(scienceCategories);
     var scienceScoreNames = ["tablets", "compasses", "gears"];
 
     var score = 0;
     var scoreNames = ["battle", "money", "wonder", "civilian", "commerce", "sciencetotal", "guild"];
+
     var totalScoreNames = scoreNames.concat(scienceScoreNames);
     totalScoreNames.push("total");
-    $("#playernumbers").val(players);
+
+    $("#playernumber").val(currentPlayers);
+    var rowNames = totalScoreNames;
+    rowNames.push("name");
 
     $("#calculate").click(function () {
-        for (var i = 0; i < players; i++) {
+        for (var i = 0; i < currentPlayers; i++) {
             var playerNumber = "#player" + (i + 1);
          
             var total = calculatePlayerScore(playerNumber);
@@ -28,10 +33,28 @@
     });
 
     $("#clear").click(function () {
-        for (var j = 0; j < players; j++) {
+        for (var j = 0; j < maxPlayers; j++) {
             var currentPlayer = j + 1;
             for (var k = 0; k < totalScoreNames.length; k++) {
                 $("#player" + currentPlayer + totalScoreNames[k]).val("");
+            }
+        }
+    })
+
+    $("#playernumber").change(function () {
+        currentPlayers = $("#playernumber").val();
+        for (var n = 0; n < maxPlayers; n++) {
+            var currentPlayer = n + 1;
+            for (var o = 0; o < rowNames.length; o++) {
+                var inputID = "#player" + currentPlayer + rowNames[o];
+                if (currentPlayer > currentPlayers) {
+                    $(inputID).parent().addClass("grayedout");
+                    $(inputID).attr("readonly", true);
+                    $(inputID).val("");
+                } else {
+                    $(inputID).parent().removeClass("grayedout");
+                    $(inputID).removeAttr("readonly");
+                }
             }
         }
     })
